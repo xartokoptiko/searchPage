380b9d9c6dab5158a563559b1b0b0fcf8d59a527
lampros <lamproskarachristos@yahoo.com>
2024-04-14 00:05:06 +0300

    added search function

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,20 @@
 /*
-* @author Lampros Karachristos
-*/
-
+ * @author Lampros Karachristos
+ */
 
 // initialize functions here !
 
+function searchGoogle() {
+  var searchTerm = document.getElementById("searchInput").value;
+  var googleUrl =
+    "https://www.google.com/search?q=" + encodeURIComponent(searchTerm);
+  window.open(googleUrl, "_blank");
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      searchGoogle();
+    }
+  }
 
 // Call functions here !
